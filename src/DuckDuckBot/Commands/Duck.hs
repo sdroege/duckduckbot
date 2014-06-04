85fb32ebe7413632c7a58550ecda5de0b9662994
Sebastian Dröge <sebastian@centricular.com>
2014-06-04 16:28:58 +0200

    Make command handling code a bit more readable with helper functions and variables

--- src/DuckDuckBot/Commands/Duck.hs
+++ src/DuckDuckBot/Commands/Duck.hs
@@ -15,30 +15,50 @@ import qualified Network.IRC as IRC
 import System.Random
 
 duckCommandHandler :: MessageHandler
-duckCommandHandler cIn cOut = do
-    channel <- asks pluginEnvChannel
-    forever $ do
-        msg <- liftIO $ readChan cIn
-        case msg of
-            InIRCMessage m@(IRC.Message { IRC.msg_command = "PRIVMSG" }) -> handlePrivmsg m (UB.fromString channel)
-            Quit                                                         -> return () -- TODO: Handle quit
-            _                                                            -> return ()
-        where
-            handlePrivmsg (IRC.Message {IRC.msg_prefix = prefix, IRC.msg_params = target:s:[]}) channel | "!duck" `B.isPrefixOf` s = handleDuck prefix target channel
-            handlePrivmsg _ _                                                                                                      = return ()
+duckCommandHandler cIn cOut = forever $ do
+    msg <- liftIO $ readChan cIn
+    case msg of
+        InIRCMessage m | isDuckCommand m -> handleDuck m
+        Quit                             -> return () -- TODO: Handle quit
+        _                                -> return ()
+    where
+        isDuckCommand msg = command == "PRIVMSG" && length params == 2 && "!duck" `B.isPrefixOf` s
+                            where
+                                command = IRC.msg_command msg
+                                params = IRC.msg_params msg
+                                s = head (tail params)
 
-            handleDuck _ target channel | target == channel   = sendDuck target
-            handleDuck (Just (IRC.NickName n _ _)) _ _        = sendDuck n
-            handleDuck _ _ _                                  = return ()
+        handleDuck msg = when (target /= B.empty) $ sendDuck target
+                         where
+                             target = getPrivMsgReplyTarget msg
 
-            sendDuck target = do
-                                 d <- liftIO duck
-                                 liftIO $ writeChan cOut (duckMessage target (UB.fromString d))
-            duckMessage target d = OutIRCMessage IRC.Message { IRC.msg_prefix = Nothing,
-                                                               IRC.msg_command = "PRIVMSG",
-                                                               IRC.msg_params = [target, d]
-                                                             }
+        -- TODO: This should be in some utils module
+        getPrivMsgReplyTarget msg | isChannel target  = target
+                                  | isNickName prefix = getNickName prefix
+                                  | otherwise         = B.empty
+                                  where
+                                      target = head (IRC.msg_params msg)
+                                      prefix = IRC.msg_prefix msg
+                                      isChannel = (UB.fromString "#" `B.isPrefixOf`)
+                                      isNickName p = case p of
+                                                         (Just (IRC.NickName {})) -> True
+                                                         _                        -> False
+                                      getNickName p = case p of
+                                                         (Just (IRC.NickName n _ _)) -> n
+                                                         _                           -> B.empty
 
+        sendDuck target = do
+                             d <- liftIO duck
+                             liftIO $ writeChan cOut (duckMessage target (UB.fromString d))
+
+        duckMessage target d = OutIRCMessage IRC.Message { IRC.msg_prefix = Nothing,
+                                                           IRC.msg_command = "PRIVMSG",
+                                                           IRC.msg_params = [target, d]
+                                                         }
+
+--
+-- The actual duck string creation
+--
 leftBeaks, rightBeaks :: [String]
 leftBody, rightBody :: [String]
 heads :: [String]
--- src/DuckDuckBot/Commands/Ping.hs
+++ src/DuckDuckBot/Commands/Ping.hs
@@ -13,11 +13,17 @@ pingCommandHandler :: MessageHandler
 pingCommandHandler cIn cOut = forever $ do
     msg <- liftIO $ readChan cIn
     case msg of
-        InIRCMessage m@(IRC.Message { IRC.msg_command = "PING" }) -> handlePing m
-        Quit                                                      -> return () -- TODO: Handle quit
-        _                                                         -> return ()
+        InIRCMessage m | isPingCommand m -> handlePing m
+        Quit                             -> return () -- TODO: Handle quit
+        _                                -> return ()
     where
+        isPingCommand msg = command == "PING" && length params <= 2
+                            where
+                                command = IRC.msg_command msg
+                                params = IRC.msg_params msg
+
         handlePing   m = liftIO $ writeChan cOut (pongFromPing m)
+
         pongFromPing m = OutIRCMessage IRC.Message { IRC.msg_prefix = Nothing,
                                                      IRC.msg_command = "PONG",
                                                      IRC.msg_params = IRC.msg_params m
