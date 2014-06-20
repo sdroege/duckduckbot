e8f422fcc13e725ef9c2e48eae34453491c001d4
Sebastian Dröge <sebastian@centricular.com>
2014-06-20 19:47:03 +0200

    Add error handling for reading configuration from the environment

--- src/DuckDuckBot/Config.hs
+++ src/DuckDuckBot/Config.hs
@@ -6,21 +6,29 @@ import DuckDuckBot.Types (Config (..))
 
 import Data.Maybe
 import System.Environment
+import Control.Monad
+import Control.Exception
 
 getConfig :: IO Config
 getConfig = do
     server           <- lookupEnv "DDB_SERVER"
-    port             <- lookupEnv "DDB_PORT"
+    portString       <- lookupEnv "DDB_PORT"
     nick             <- lookupEnv "DDB_NICK"
     nickServPassword <- lookupEnv "DDB_NICKSERV_PASSWORD"
     channel          <- lookupEnv "DDB_CHANNEL"
-    useSsl           <- lookupEnv "DDB_USE_SSL"
+    useSslString     <- lookupEnv "DDB_USE_SSL"
+
+    when (isNothing server) $ error "Need to set server in $DDB_SERVER"
+    when (isNothing portString) $ error "Need to set port in $DDB_PORT"
+    port <- handle (\(SomeException _) -> error "Need to set port in $DDB_PORT") $ evaluate (read (fromJust portString) :: Int)
+    when (isNothing nick) $ error "Need to set nickname in $DDB_NICK"
+    when (isNothing channel) $ error "Need to set channel in $DDB_CHANNEL"
+
     return Config {
         cfgServer=fromJust server,
-        cfgPort=read $ fromJust port,
+        cfgPort=port,
         cfgNick=fromJust nick,
         cfgNickServPassword=nickServPassword,
         cfgChannel=fromJust channel,
-        cfgUseSsl=fromJust useSsl == "1"
+        cfgUseSsl=useSslString == Just "1"
     }
--- FIXME: Error checking
