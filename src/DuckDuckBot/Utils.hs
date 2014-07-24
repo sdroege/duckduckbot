0e184c6a6149f931fc23c350a2ac9a91827a20a0
Sebastian Dröge <sebastian@centricular.com>
2014-07-24 19:25:11 +0200

    Simplify PRIVMSG utility functions with pattern matching

--- src/DuckDuckBot/Utils.hs
+++ src/DuckDuckBot/Utils.hs
@@ -12,26 +12,17 @@ import qualified Data.ByteString.UTF8 as UB
 import qualified Network.IRC as IRC
 
 isPrivMsgCommand :: B.ByteString -> IRC.Message -> Bool
-isPrivMsgCommand c msg =    command == "PRIVMSG" && length params == 2 && (("!" `B.append` c `B.append` " ") `B.isPrefixOf` s || ("!" `B.append` c) == s)
-                            where
-                                command = IRC.msg_command msg
-                                params = IRC.msg_params msg
-                                s = head (tail params)
+isPrivMsgCommand c (IRC.Message _ "PRIVMSG" (_:s:[])) = ((commandString `B.append` " ") `B.isPrefixOf` s || commandString == s)
+                                                        where
+                                                            commandString = "!" `B.append` c
+isPrivMsgCommand _ _                                  = False
 
 getPrivMsgReplyTarget :: IRC.Message -> B.ByteString
-getPrivMsgReplyTarget msg   | isChannel target  = target
-                            | isNickName prefix = getNickName prefix
-                            | otherwise         = B.empty
-                            where
-                                target = head (IRC.msg_params msg)
-                                prefix = IRC.msg_prefix msg
-                                isChannel = (UB.fromString "#" `B.isPrefixOf`)
-                                isNickName p = case p of
-                                                    (Just (IRC.NickName {})) -> True
-                                                    _                        -> False
-                                getNickName p = case p of
-                                                    (Just (IRC.NickName n _ _)) -> n
-                                                    _                           -> B.empty
+getPrivMsgReplyTarget (IRC.Message _ "PRIVMSG" (target:_:[])) | isChannel target  = target
+                                                                                    where
+                                                                                        isChannel = (UB.fromString "#" `B.isPrefixOf`)
+getPrivMsgReplyTarget (IRC.Message (Just (IRC.NickName nickname _ _)) _ _)        = nickname
+getPrivMsgReplyTarget _                                                           = B.empty
 
 untilFalse :: (Monad m) => m Bool -> m ()
 untilFalse p = do
