951b8dcdd1c831f29a63006e4c9d2a0998191b7b
Sebastian Dröge <sebastian@centricular.com>
2014-12-20 17:35:21 +0100

    Move some code into separate modules

--- src/DuckDuckBot/Commands/Ddg.hs
+++ src/DuckDuckBot/Commands/Ddg.hs
@@ -4,25 +4,21 @@ module DuckDuckBot.Commands.Ddg (
 
 import DuckDuckBot.Types
 import DuckDuckBot.Utils
+import qualified DuckDuckBot.Commands.DdgQuery as DQ
 
 import Data.Maybe
 import Data.Char
 import qualified Data.ByteString as B
 import qualified Data.ByteString.UTF8 as UB
 
-import Data.Aeson hiding (Result)
-
 import qualified Network.HTTP.Client as HTTP
-import qualified Network.HTTP.Types.Header as HTTPH
 import qualified Network.HTTP.Client.TLS as HTTPS
 import qualified Network.URI as URI
 import qualified Network.IRC as IRC
 
 import Control.Monad
 import Control.Monad.Reader
-import Control.Applicative
 import Control.Concurrent
-import Control.Exception
 
 ddgCommandHandlerMetadata :: MessageHandlerMetadata
 ddgCommandHandlerMetadata = MessageHandlerMetadata {
@@ -60,7 +56,7 @@ handleMessage msg send = do
 handleQuery :: MessageHandlerSendMessage -> HTTP.Manager -> B.ByteString -> B.ByteString -> IO ()
 handleQuery send manager target query = do
     let queryString = UB.toString query
-    response <- doQuery manager queryString
+    response <- DQ.query manager queryString
     when (isJust response) $ do
         let answer = generateAnswer queryString (fromJust response)
         when (answer /= "") $ send (answerMessage (UB.fromString answer))
@@ -70,10 +66,7 @@ handleQuery send manager target query = do
                                               IRC.msg_params = [target, answer]
                                            }
 
-escapeQuery :: String -> String
-escapeQuery = URI.escapeURIString URI.isUnescapedInURIComponent
-
-generateAnswer :: String -> Results -> String
+generateAnswer :: String -> DQ.Results -> String
 generateAnswer q r = take n a ++ dots ++ take m b
                         where
                             maxIRCLen = 400 -- arbitrary number below 512 to allow some space for channel, prefix, etc
@@ -82,108 +75,17 @@ generateAnswer q r = take n a ++ dots ++ take m b
                             n = max 0 (maxIRCLen-m)
                             dots = if n < length a then "..." else ""
 
-generateAnswer' :: String -> Results -> (String, String)
-generateAnswer' q r | resultsAnswer r /= ""                          = (resultsAnswer r, " (http://ddg.gg/" ++ escapeQuery q ++ ")")
-                    | resultsAbstractText r /= ""                    = (resultsAbstractText r, " (" ++ resultsAbstractSource r ++ ", http://ddg.gg/" ++ escapeQuery q ++ ")")
-                    | resultsDefinition r /= ""                      = (resultsDefinition r, " (" ++ resultsDefinitionSource r ++ ", http://ddg.gg/" ++ escapeQuery q ++ ")")
-                    | firstResultText (resultsRelatedTopics r) /= "" = (firstResultText (resultsRelatedTopics r), " (" ++ resultsAbstractSource r ++ ", http://ddg.gg/" ++ escapeQuery q ++ ")")
-                    | resultsRedirect r /= ""                        = (resultsRedirect r, "")
+generateAnswer' :: String -> DQ.Results -> (String, String)
+generateAnswer' q r | DQ.resultsAnswer r /= ""                          = (DQ.resultsAnswer r, " (http://ddg.gg/" ++ escapedQuery ++ ")")
+                    | DQ.resultsAbstractText r /= ""                    = (DQ.resultsAbstractText r, " (" ++ DQ.resultsAbstractSource r ++ ", http://ddg.gg/" ++ escapedQuery ++ ")")
+                    | DQ.resultsDefinition r /= ""                      = (DQ.resultsDefinition r, " (" ++ DQ.resultsDefinitionSource r ++ ", http://ddg.gg/" ++ escapedQuery ++ ")")
+                    | firstResultText (DQ.resultsRelatedTopics r) /= "" = (firstResultText (DQ.resultsRelatedTopics r), " (" ++ DQ.resultsAbstractSource r ++ ", http://ddg.gg/" ++ escapedQuery ++ ")")
+                    | DQ.resultsRedirect r /= ""                        = (DQ.resultsRedirect r, "")
 
-                    | otherwise                                      = ("http://ddg.gg/" ++ escapeQuery q, "")
+                    | otherwise                                         = ("http://ddg.gg/" ++ escapedQuery, "")
                  where
-                     firstResultText ((Result { resultText=rt }):_) = rt
-                     firstResultText (_:rs)                         = firstResultText rs
-                     firstResultText _                              = ""
-
-data Results = Results {
-    resultsAbstract :: String,
-    resultsAbstractText :: String,
-    resultsAbstractSource :: String,
-    resultsAbstractURL :: String,
-    resultsImage :: String,
-    resultsHeading :: String,
-    resultsAnswer :: String,
-    resultsRedirect :: String,
-    resultsAnswerType :: String,
-    resultsDefinition :: String,
-    resultsDefinitionSource :: String,
-    resultsDefinitionURL :: String,
-    resultsRelatedTopics :: [Result],
-    resultsResults :: [Result],
-    resultsType :: String
-} deriving (Show, Eq)
-
-data Result = Result {
-    resultResult :: String,
-    resultIcon :: Maybe Icon,
-    resultFirstURL :: String,
-    resultText :: String
-} | ResultGroup {
-    resultGroupTopics :: [Result],
-    resultGroupName :: String
-} deriving (Show, Eq)
-
-data Icon = Icon {
-    iconURL :: String,
-    iconHeight :: Int,
-    iconWidth :: Int
-} deriving (Show, Eq)
-
-instance FromJSON Results where
-    parseJSON (Object v) = Results <$>
-                                v .: "Abstract" <*>
-                                v .: "AbstractText" <*>
-                                v .: "AbstractSource" <*>
-                                v .: "AbstractURL" <*>
-                                v .: "Image" <*>
-                                v .: "Heading" <*>
-                                v .: "Answer" <*>
-                                v .: "Redirect" <*>
-                                v .: "AnswerType" <*>
-                                v .: "Definition" <*>
-                                v .: "DefinitionSource" <*>
-                                v .: "DefinitionURL" <*>
-                                v .: "RelatedTopics" <*>
-                                v .: "Results" <*>
-                                v .: "Type"
-    parseJSON _          = mzero
-
-instance FromJSON Result where
-    parseJSON (Object v) = Result <$>
-                                v .: "Result" <*>
-                                v .: "Icon" <*>
-                                v .: "FirstURL" <*>
-                                v .: "Text"
-                            <|>
-                           ResultGroup <$>
-                                v .: "Topics" <*>
-                                v .: "Name"
-    parseJSON _          = mzero
-
-instance FromJSON Icon where
-    parseJSON (Object v) = Icon <$>
-                                v .: "URL" <*>
-                                -- Height and width can be an Int or ""
-                                intOrString "Height" <*>
-                                intOrString "Width"
-                           where
-                                intOrString s = v .: s <|> (stringTo0 <$> (v .: s))
-                                stringTo0 :: String -> Int
-                                stringTo0 _ = 0
-    parseJSON _          = mzero
-
-doQuery :: HTTP.Manager -> String -> IO (Maybe Results)
-doQuery m s = do
-    let q   = escapeQuery s
-        url = "https://api.duckduckgo.com/?q=" ++ q ++ "&format=json&no_redirect=1&t=ddb&no_html=1"
-    -- Catch all exceptions here and return nothing
-    -- Better do nothing than crashing when we can't do the HTTP request
-    handle (\(SomeException e) -> print ("Exception while handling Ddg request \"" ++ s ++ "\": " ++ show e) >> return Nothing) $ do
-        baseReq <- HTTP.parseUrl url
-        let headers = (HTTPH.hConnection, "Keep-Alive") : HTTP.requestHeaders baseReq
-            req  = baseReq { HTTP.requestHeaders=headers }
-        resp <- HTTP.httpLbs req m
-        let d = decode (HTTP.responseBody resp) :: Maybe Results
-        HTTP.responseClose resp
-        return d
+                     firstResultText ((DQ.Result { DQ.resultText=rt }):_) = rt
+                     firstResultText (_:rs)                               = firstResultText rs
+                     firstResultText _                                    = ""
+                     escapedQuery                                         = URI.escapeURIString URI.isUnescapedInURIComponent q
 
---
+++ src/DuckDuckBot/Core.hs
@@ -0,0 +1,248 @@
+module DuckDuckBot.Core
+    ( run
+    ) where
+
+import DuckDuckBot.Types
+import DuckDuckBot.Utils
+import DuckDuckBot.Compat
+
+import DuckDuckBot.Commands.Ping
+import DuckDuckBot.Commands.Duck
+import DuckDuckBot.Commands.Ddg
+import DuckDuckBot.Commands.Quotes
+import DuckDuckBot.Commands.LinkTitle
+
+import Data.Char
+import Data.Maybe
+import Data.List
+import qualified Data.ByteString.UTF8 as UB
+import qualified Data.ByteString as B
+
+import Control.Exception
+import Control.Monad
+import Control.Monad.Reader
+import Control.Concurrent
+
+import qualified Network.IRC as IRC
+
+-- Run everything. We run the read loop in the main thread
+-- and everything else in other threads.
+--
+-- Exceptions from the read and write loops are supposed to get
+-- here and cause the application to stop, and the same goes for
+-- all unrecoverable exceptions from command handlers
+run :: Config -> IO ()
+run config = bracket (setup config) shutdown loop
+
+messageHandlers :: [MessageHandlerMetadata]
+messageHandlers = [pingCommandHandlerMetadata, duckCommandHandlerMetadata, ddgCommandHandlerMetadata, quotesCommandHandlerMetadata, linkTitleCommandHandlerMetadata]
+
+setup :: Config -> IO Env
+setup config = do
+    connection <- newConnection (cfgServer config) (cfgPort config) (cfgUseSsl config)
+
+    inChan  <- newChan :: IO (Chan InMessage)
+    outChan <- newChan :: IO (Chan OutMessage)
+
+    authUser <- newMVar (Nothing :: Maybe IRC.Prefix)
+
+    let env = Env {
+        envConfig=config,
+        envConnection=connection,
+        envInChan=inChan,
+        envOutChan=outChan,
+        envAuthUser=authUser
+    }
+
+    return env
+
+shutdown :: Env -> IO ()
+shutdown env = closeConnection (envConnection env)
+
+loop :: Env -> IO ()
+loop env = do
+    let connection = envConnection env
+        inChan = envInChan env
+        outChan = envOutChan env
+        config = envConfig env
+        authUser = envAuthUser env
+
+    threads <- newMVar []
+    let forkThread io = do
+                            mvar <- newEmptyMVar
+                            ts   <- takeMVar threads
+                            putMVar threads (mvar:ts)
+                            void (forkFinally io (\_ -> putMVar mvar ()))
+        waitForThreads = do
+                            ts   <- takeMVar threads
+                            case ts of
+                                []    -> return ()
+                                m:ms  -> do
+                                            putMVar threads ms
+                                            takeMVar m
+                                            waitForThreads
+
+    liftIO . forkThread $ runReaderT (writeLoop outChan connection) env
+
+    -- Start all message handlers here
+    let messageHandlerEnv   = MessageHandlerEnv { messageHandlerEnvServer  = cfgServer config,
+                                                  messageHandlerEnvNick    = cfgNick config,
+                                                  messageHandlerEnvChannel = cfgChannel config,
+                                                  messageHandlerEnvIsAuthUser = isAuthUser authUser
+                                                }
+        runMessageHandler m = do
+                                 mChan <- dupChan inChan
+                                 runReaderT (m mChan outChan) messageHandlerEnv
+    mapM_ (liftIO . forkThread . runMessageHandler . messageHandlerMetadataHandler) messageHandlers
+
+    -- Special handler for !help without arguments
+    liftIO $ forkThread $ runMessageHandler helpCommandHandler
+
+    -- Special handler for !auth
+    when (isJust (cfgAuthPassword config)) $
+        liftIO $ forkThread $ runMessageHandler (authCommandHandler (fromJust (cfgAuthPassword config)) authUser)
+
+    runReaderT (readLoop messageHandlerEnv connection inChan outChan) env
+
+    waitForThreads
+
+--
+-- Reads data from the handle, converts them to IRC.Messages
+-- and writes them to the envInChan for all command handlers
+-- to consume them
+--
+readLoop :: MessageHandlerEnv -> Connection -> Chan InMessage -> Chan OutMessage -> EnvReader IO ()
+readLoop env con inChan outChan = runConnectionReader con (messageReader env inChan outChan)
+
+--
+-- Reads IRC messages from the socket and passes them to all
+-- the command handler
+--
+-- We don't catch any exceptions here. If writing to the
+-- connection fails we will stop the application
+--
+messageReader :: MessageHandlerEnv -> Chan InMessage -> Chan OutMessage -> ConnectionReader (EnvReader IO) ()
+messageReader env inChan outChan = untilFalse $ do
+    msg <- getMessage
+    liftIO $ writeChan inChan (InIRCMessage msg)
+
+
+    case msg of
+        (IRC.Message (Just prefix) "PRIVMSG" (_:"!quit":[])) -> do
+                                                                    a <- liftIO $ messageHandlerEnvIsAuthUser env prefix
+                                                                    if a then
+                                                                        do
+                                                                            liftIO $ writeChan inChan Quit
+                                                                            let quitMessage = IRC.Message Nothing "QUIT" []
+                                                                            liftIO $ writeChan outChan (OutIRCMessage quitMessage)
+                                                                            return False
+                                                                    else
+                                                                        return True
+
+        _                                                    -> return True
+
+--
+-- Reads Messages from the envOutChan and does whatever
+-- is necessary to handle them, i.e. write IRC.Messages
+-- to the handle or quit or ...
+--
+-- We don't catch any exceptions here. If writing to the
+-- connection fails we will stop the application
+--
+writeLoop :: Chan OutMessage -> Connection -> EnvReader IO ()
+writeLoop chan con = do
+    config <- asks envConfig
+    -- Send initial commands
+    let sendNickCommand = write "NICK" [cfgNick config]
+        sendUserCommand = write "USER" [cfgNick config, "0", "*", "duck duck bot"]
+        sendNickServPassword = case config of
+                                    (Config { cfgNickServPassword=Just pw}) -> write "PRIVMSG" ["NickServ", "IDENTIFY " ++ pw]
+                                    _                                       -> return ()
+        sendJoinCommand = write "JOIN" [cfgChannel config]
+        write command params = liftIO $ putMessageConnection con (IRC.Message Nothing command (convertParams params))
+        convertParams = map UB.fromString
+
+    sendNickCommand
+    sendUserCommand
+    sendNickServPassword
+    sendJoinCommand
+
+    untilFalse $ do
+        msg <- liftIO $ readChan chan
+        -- Catch exceptions that might occur from evaluation
+        -- a lazy, non-evaluated message here
+        evMsg <- liftIO (try (evaluate msg) :: IO (Either SomeException OutMessage))
+        case evMsg of
+            Right (OutIRCMessage m) -> do
+                                            liftIO $ putMessageConnection con m
+                                            case m of
+                                                m' | IRC.msg_command m' == "QUIT" -> return False
+                                                _                                 -> return True
+            Left e                  -> do
+                                            liftIO $ print ("Exception in writeLoop: " ++ show e)
+                                            return True
+
+
+--
+-- Special handler for the !help command without parameters
+--
+helpCommandHandler :: MessageHandler
+helpCommandHandler = messageHandlerLoop id handleHelpMessage
+
+handleHelpMessage :: IRC.Message -> MessageHandlerSendMessage -> MessageHandlerEnvReader IO ()
+handleHelpMessage msg send =
+    case msg of
+        (IRC.Message _ "PRIVMSG" (_:"!help":[])) -> when (target /= B.empty) $ sendHelp target
+                                                        where
+                                                            target = getPrivMsgReplyTarget msg
+        _                                        -> return ()
+    where
+        sendHelp target = liftIO $ send (helpMessage target)
+        helpMessage target = IRC.Message  {   IRC.msg_prefix = Nothing,
+                                              IRC.msg_command = "PRIVMSG",
+                                              IRC.msg_params = [target, helpString]
+                                          }
+                             where
+                                helpString = UB.fromString $ "Available commands: " ++ commands -- ++ ". Try !help COMMAND"
+                                commands = mapIntercalate (handlerCommands . messageHandlerMetadataCommands) ", " messageHandlers
+                                handlerCommands = mapIntercalate id ", "
+                                mapIntercalate f s l = intercalate s (mapMaybe (\a -> let r = f a in if null (dropWhile isSpace r) then Nothing else Just r) l)
+
+--
+-- Special handler for the !auth command
+--
+authCommandHandler :: String -> MVar (Maybe IRC.Prefix) -> MessageHandler
+authCommandHandler pw authUser = messageHandlerLoop id (handleAuthMessage pw authUser)
+
+handleAuthMessage :: String -> MVar (Maybe IRC.Prefix) -> IRC.Message -> MessageHandlerSendMessage -> MessageHandlerEnvReader IO ()
+handleAuthMessage pw authUser msg send =
+    case msg of
+        (IRC.Message (Just prefix@(IRC.NickName n _ _)) "PRIVMSG" (_:s:[])) | "!auth " `B.isPrefixOf` s -> handleAuth prefix n s
+        _                                                                          -> return ()
+    where
+        handleAuth prefix n s = do
+                                    let p = UB.toString (B.drop 6 s)
+                                    if p == pw then
+                                        do
+                                            old <- liftIO $ swapMVar authUser (Just prefix)
+                                            sendAuthMessage n ("Authenticated " `B.append` n)
+                                            case old of
+                                                Just (IRC.NickName n' _ _) -> sendAuthMessage n' ("Authenticated " `B.append` n)
+                                                _                          -> return ()
+                                    else
+                                        sendAuthMessage n "Authentication failed"
+
+        sendAuthMessage n m = liftIO $ send (authMessage n m)
+        authMessage n m = IRC.Message {   IRC.msg_prefix = Nothing,
+                                          IRC.msg_command = "NOTICE",
+                                          IRC.msg_params = [n, m]
+                                      }
+
+isAuthUser :: MVar (Maybe IRC.Prefix) -> IRC.Prefix -> IO Bool
+isAuthUser m n = do
+    a <- readMVar m
+    case a of
+        Just p -> return (n == p)
+        _      -> return False
+
+
