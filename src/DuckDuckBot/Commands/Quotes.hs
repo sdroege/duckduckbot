5160dab907e52d10debe732c01a527d814588910
Sebastian Dröge <sebastian@centricular.com>
2014-12-24 12:04:54 +0100

    Add new Quotes database schema and migration
    This one now has migration paths for the automatically, non-TH derived
    SafeCopy instances which fail to compile with newer GHC.

--- src/DuckDuckBot/Commands/Quotes.hs
+++ src/DuckDuckBot/Commands/Quotes.hs
@@ -38,25 +38,83 @@ import System.Directory
 import System.Environment.XDG.BaseDir
 import System.Locale
 
-newtype QuoteId = QuoteId { unQuoteId :: Integer }
+newtype QuoteId_v0 = QuoteId_v0 { unQuoteId_v0 :: Integer }
     deriving (Eq, Ord, Data, Enum, Typeable, SafeCopy)
+
+newtype QuoteId = QuoteId { unQuoteId :: Integer }
+    deriving (Eq, Ord, Data, Enum, Typeable)
+$(deriveSafeCopy 1 'extension ''QuoteId)
+
+instance Migrate QuoteId where
+    type MigrateFrom QuoteId = QuoteId_v0
+    migrate (QuoteId_v0 i) = QuoteId i
+
+newtype Author_v0 = Author_v0 T.Text
+    deriving (Eq, Ord, Data, Typeable, SafeCopy)
+
+newtype QuoteAuthor = QuoteAuthor {unQuoteAuthor :: T.Text}
+    deriving (Eq, Ord, Data, Typeable)
+$(deriveSafeCopy 1 'extension ''QuoteAuthor)
+
+instance Migrate QuoteAuthor where
+    type MigrateFrom QuoteAuthor = Author_v0
+    migrate (Author_v0 a) = QuoteAuthor a
+
+newtype QuoteTime = QuoteTime {unQuoteTime :: UTCTime}
+    deriving (Eq, Ord, Data, Typeable)
+$(deriveSafeCopy 1 'extension ''QuoteTime)
+
+instance Migrate QuoteTime where
+    type MigrateFrom QuoteTime = UTCTime
+    migrate t = QuoteTime t
+
+newtype QuoteText = QuoteText {unQuoteText :: T.Text}
+    deriving (Eq, Ord, Data, Typeable)
+$(deriveSafeCopy 1 'extension ''QuoteText)
+
+instance Migrate QuoteText where
+    type MigrateFrom QuoteText = T.Text
+    migrate t = QuoteText t
+
+data Quote_v0 = Quote_v0 {
+    quoteId_v0 :: QuoteId_v0,
+    quoteTime_v0 :: UTCTime,
+    quoteAuthor_v0 :: T.Text,
+    quoteText_v0 :: T.Text
+} deriving (Eq, Ord, Data, Typeable)
+
+$(deriveSafeCopy 0 'base ''Quote_v0)
 
 data Quote = Quote {
     quoteId :: QuoteId,
-    quoteTime :: UTCTime,
-    quoteAuthor :: T.Text,
-    quoteText :: T.Text
+    quoteTime :: QuoteTime,
+    quoteAuthor :: QuoteAuthor,
+    quoteText :: QuoteText
 } deriving (Eq, Ord, Data, Typeable)
 
-$(deriveSafeCopy 0 'base ''Quote)
+$(deriveSafeCopy 1 'extension ''Quote)
 
-newtype Author = Author T.Text
-    deriving (Eq, Ord, Data, Typeable, SafeCopy)
+instance Migrate Quote where
+    type MigrateFrom Quote = Quote_v0
+    migrate (Quote_v0 i t a q) = Quote (QuoteId . unQuoteId_v0 $ i) (QuoteTime t) (QuoteAuthor a) (QuoteText q)
+
+instance Indexable Quote_v0 where
+    empty = ixSet
+        [ ixFun $ \q -> [quoteId_v0 q],
+          ixFun $ \q -> [Author_v0 (quoteAuthor_v0 q)]
+        ]
+
+data Quotes_v0 = Quotes_v0 {
+    nextQuoteId_v0 :: QuoteId_v0,
+    quotes_v0 :: IxSet Quote_v0
+} deriving (Data, Typeable)
+
+$(deriveSafeCopy 0 'base ''Quotes_v0)
 
 instance Indexable Quote where
     empty = ixSet
         [ ixFun $ \q -> [quoteId q],
-          ixFun $ \q -> [Author (quoteAuthor q)]
+          ixFun $ \q -> [quoteAuthor q]
         ]
 
 data Quotes = Quotes {
@@ -64,7 +122,15 @@ data Quotes = Quotes {
     quotes :: IxSet Quote
 } deriving (Data, Typeable)
 
-$(deriveSafeCopy 0 'base ''Quotes)
+$(deriveSafeCopy 1 'extension ''Quotes)
+
+instance Migrate Quotes where
+    type MigrateFrom Quotes = Quotes_v0
+    migrate (Quotes_v0 i qs) = Quotes i' qs'
+        where
+            i'  = QuoteId . unQuoteId_v0 $ i
+            qs' = IX.fromList . map migrateQuotes_v0 . IX.toList $ qs
+            migrateQuotes_v0 q0 = migrate q0
 
 initialQuotesState :: Quotes
 initialQuotesState = Quotes { nextQuoteId = QuoteId 1, quotes = IX.empty }
@@ -74,9 +140,9 @@ addQuote time author text = do
     qs <- get
     let quote = Quote {
             quoteId     = nextQuoteId qs,
-            quoteTime   = time,
-            quoteAuthor = author,
-            quoteText   = text }
+            quoteTime   = QuoteTime time,
+            quoteAuthor = QuoteAuthor author,
+            quoteText   = QuoteText text }
 
     put $ qs {  nextQuoteId = succ (nextQuoteId qs),
                 quotes      = insert quote (quotes qs)
@@ -103,7 +169,7 @@ getNumQuotes = do
 getQuotesByAuthor :: T.Text -> Query Quotes [Quote]
 getQuotesByAuthor author = do
     qs <- ask
-    let q = quotes qs @= Author author
+    let q = quotes qs @= QuoteAuthor author
 
     return (toList q)
 
@@ -144,9 +210,9 @@ getQuoteString acid author = do
     case q of
         Just q' -> do
                     let qId = show (unQuoteId (quoteId q'))
-                        author' = quoteAuthor q'
-                        time = formatTime defaultTimeLocale "%F %R UTC" (quoteTime q')
-                        text = quoteText q'
+                        author' = unQuoteAuthor $ quoteAuthor q'
+                        time = formatTime defaultTimeLocale "%F %R UTC" (unQuoteTime . quoteTime $ q')
+                        text = unQuoteText $ quoteText q'
                         s = "-- Quote " ++ qId ++ ", " ++ T.unpack author' ++ " at " ++ time
                     return (Just (T.unpack text, s))
         _       -> return Nothing
